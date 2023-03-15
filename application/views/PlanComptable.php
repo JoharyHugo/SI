6c065460d3911507829768f3dcaef98c41df7013
JoharyHugo <joharyny@gmail.com>
2023-03-15 03:21:56 +0100

    Create PlanComptable.php

---
+++ application/views/PlanComptable.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/styles.css"); ?>">
+    <title>Document</title>
+</head>
+<body>
+    <form action="#" method="post">
+            <center><h1>Plan Comptable</h1></center>
+            <label for="numero">numero:</label>
+            <input type="number" name="numero" id="numero">
+            <br><br>    
+            <label for="nom">Nom :</label>
+            <input type="text" name="nom" id="nom">
+            <br><br>
+            <input type="submit" value="Valider">
+    </form>
+</body>
+</html>
