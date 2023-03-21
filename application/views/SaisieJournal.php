07cc0d355df52953df2dcdce0095d406af08fee6
JoharyHugo <joharyny@gmail.com>
2023-03-21 15:56:09 +0100

    Login

---
+++ application/views/SaisieJournal.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+    <title>Journal</title>
+</head>
+<body>
+<div class="container">
+        <div class="row cont" style="width: 1700px;">
+            <div class="col-md-6" >
+                <h2 style="margin-left: 250px;">Saisiser votre Journal</h2>
+               <form action="#" method="post">
+               <div class="container sp1">
+               <table class="normal">
+                <tr>
+                    <th>Jour</th>
+                    <th>Piece</th>
+                    <th>Reference</th>
+                    <th>Compte</th>
+                    <th>Compte_Tiers</th>
+                    <th>Typage</th>
+                    <th>Libelle</th>
+                    <th>Devise</th>
+                    <th>Montant_Devise</th>
+                    <th>Debit</th>
+                    <th>Credit</th>
+                </tr>
+                <tr>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                    <td><input type="text"></td>
+                </tr>
+               </table>
+               </form>
+            </div>
+           
+         
+        </div>
+           
+</div>
+
+  
+</body>
+</html>
