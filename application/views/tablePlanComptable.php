b54b6b2e12cab12fc6f9d9428511191cd66eca32
JoharyHugo <joharyny@gmail.com>
2023-03-21 22:05:13 +0100

    Update tablePlanComptable.php

--- application/views/tablePlanComptable.php
+++ application/views/tablePlanComptable.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
   <head>
-  	<title>PlanComptable<</title>
+  	<title>Plan Comptable<</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -17,7 +17,7 @@
 		<div class="container">
 			<div class="row justify-content-center">
 				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">PlanComptable</h2>
+					<h2 class="heading-section">Plan Comptable</h2>
 				</div>
 			</div>
 			<div class="row">
