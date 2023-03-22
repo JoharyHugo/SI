1f044fc1f7c66f4082c5d9a5aca5b09673441cc4
JoharyHugo <joharyny@gmail.com>
2023-03-22 05:22:20 +0100

    Journal

---
+++ application/views/JourneauxAffichage.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/bootstrap/css/bootstrap.min.css"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+    <title>Journal</title>
+</head>
+<body style="background-image: url('<?php echo base_url("assets/images/img2.jpg"); ?>'); background-size: cover;;background-repeat: no-repeat; ">
+    <center><h2 style="margin-top: 50px;">Votre Journal</h2></center>
+    <br>
+    <hr>
+<div class="container sp1" style="margin-right: 400px;">
+        
+        <div class="row cont" style="width: 1700px;">
+        <table class="normal">
+                <tr>
+                    <th>Jour</th>
+                    <th>Piece</th>
+                    <th>Reference</th>
+                    <th>Compte</th>
+                    <th>Compte_Tiers</th>
+                    <th>Typage</th>
+                    <th>Libelle</th>
+                    <th>Devise</th>
+                    <th>Montant_Devise</th>
+                    <th>Debit</th>
+                    <th>Credit</th>
+                </tr>
+                <tr>
+                    <td >9</td>
+                    <td>1</td>
+                    <td>BOABOA/46616</td>
+                    <td>401000</td>
+                    <td>Feno</td>
+                    <td>AC</td>
+                    <td>Achat</td>
+                    <td>Ariary</td>
+                    <td>15000</td>
+                    <td>15000</td>
+                    <td>15000</td>
+                </tr>        </table>
+          
+            </div>
+           
+         
+        </div>
+           
+</div>
+
+  
+</body>
+</html>
