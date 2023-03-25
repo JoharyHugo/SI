c332822b6fa344d9fb23c22c69655625950968e8
JoharyHugo <joharyny@gmail.com>
2023-03-25 16:34:05 +0100

    acceuil

---
+++ application/views/accueil.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Page d'accueil</title>
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/accueil.css"); ?>">
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">Journal</a></li>
+          <li><a href="#">Plan Compataple</a></li>
+          <li><a href="#">Plan Tiers</a></li>
+          <li><a href="#">Grand Livre</a></li>
+          <li><a href="#">Balance</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <h1>Bienvenue</h1>
+      <div id="info">
+        phgozgnpzh
+      </div>
+    </main>
+  </body>
+</html>
