94b28ff83fae91e7a3c382121c0e90e7238cb0e8
JoharyHugo <joharyny@gmail.com>
2023-04-18 17:46:59 +0100

    dz

---
+++ application/views/Resultat.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Slick Tables</title>
+  <link rel="stylesheet" href="<?php echo base_url("assets/css/resultat.css"); ?>">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<table>
+  <thead>
+    <tr>
+      <th>
+      <th>Compte
+      <th>Date Fin Exercice N
+      <th>Date Fin Exercice N-1
+  </thead>
+  <tbody>
+    <tr>
+      <td>1
+      <td>Malcolm
+      <td>Reynolds
+      <td>Mal, Cap'n
+      
+    <tr>
+      <td>2
+      <td>Zoe
+      <td>Washburn
+      <td>Zoe
+      
+        <tr>
+          <td>Production de l'exercice
+          <td colspan="3">
+          <td>
+          <td>
+          
+    
+    <tr>
+      <td>3
+      <td>Hoban
+      <td>Washburn
+      <td>Wash
+        <tr>
+          <td>Consommation de l'exercice
+          <td colspan="3">
+          <td>
+          <td>
+    <tr>
+      <td>4
+      <td>Kaylee
+      <td>Frye
+      <td>Kaylee
+        <tr>
+          <td>Valeur Ajouter d'exploitation
+          <td colspan="3">
+          <td>
+          <td>
+    <tr>
+      <td>5
+      <td>Jayne
+      <td>Cobb
+      <td>Jayne
+        <tr>
+          <td> EXCEDENT BRUT D’EXPLOITATION 
+          <td colspan="3">
+          <td>
+          <td>
+        <tr>
+          <td>6
+          <td>Jayne
+          <td>Cobb
+          <td>Jayne
+            <tr>
+          <td>Resultat Operationnel
+           <td colspan="3">
+             <td>
+             <td>
+            <tr>
+                  <td>6
+                  <td>Jayne
+                  <td>Cobb
+                  <td>Jayne
+                    <tr>
+                      <td> Resultat Financier 
+                      <td colspan="3">
+                      <td>
+                      <td>
+                      <tr>
+                          <td> Resultat Avnt Impot
+                          <td colspan="3">
+                          <td>
+                          <td>
+  </tbody>
+</table>
+<!-- partial -->
+  
+</body>
+</html>
