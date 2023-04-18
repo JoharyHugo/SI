34a073befe59ebbb3b323115af5e3f41af76803f
JoharyHugo <joharyny@gmail.com>
2023-04-18 16:38:44 +0100

    add Actif

---
+++ application/views/Actif.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/actif.css"); ?>">
+    <title>Document</title>
+</head>
+<body>
+   <div class="container">
+    <table>
+        <thead>
+          <tr>
+            <th >Actif</th>
+            <th>Compte</th>
+            <th colspan="3">Date Fin exercice N</th>
+            <th>Date Fin exercice N-1</th>
+          </tr>
+          <tr>
+            <th></th>
+            <th></th>
+            <th>Brut</th>
+            <th>Amort/Prov</th>
+            <th>Net</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Ligne 1, colonne 1</td>
+            <td>Ligne 1, sous-colonne 1</td>
+            <td>Ligne 1, sous-colonne 2</td>
+            <td>Ligne 1, sous-colonne 3</td>
+            <td>Ligne 1, colonne 3</td>
+            <td>Ligne 1, colonne 3</td>
+          </tr>
+          <tr>
+            <td>Ligne 2, colonne 1</td>
+            <td>Ligne 2, sous-colonne 1</td>
+            <td>Ligne 2, sous-colonne 2</td>
+            <td>Ligne 2, sous-colonne 3</td>
+            <td>Ligne 2, colonne 3</td>
+            <td>Ligne 1, colonne 3</td>
+          </tr>
+          <tr>
+            <td>Ligne 3, colonne 1</td>
+            <td>Ligne 3, sous-colonne 1</td>
+            <td>Ligne 3, sous-colonne 2</td>
+            <td>Ligne 3, sous-colonne 3</td>
+            <td>Ligne 3, colonne 3</td>
+            <td>Ligne 1, colonne 3</td>
+          </tr>
+          <tr>
+            <td>Ligne 3, colonne 1</td>
+            <td>Ligne 3, sous-colonne 1</td>
+            <td>Ligne 3, sous-colonne 2</td>
+            <td>Ligne 3, sous-colonne 3</td>
+            <td>Ligne 3, colonne 3</td>
+            <td>Ligne 1, colonne 3</td>
+          </tr>
+          <tr>
+            <td colspan="6">Total Actif Non Courants</td>
+          </tr>
+          <tr>
+            <td>Ligne 3, colonne 1</td>
+            <td>Ligne 3, sous-colonne 1</td>
+            <td>Ligne 3, sous-colonne 2</td>
+            <td>Ligne 3, sous-colonne 3</td>
+            <td>Ligne 3, colonne 3</td>
+            <td>Ligne 1, colonne 3</td>
+          </tr>
+          <tr>
+            <td colspan="6">Total Actif  Courants</td>
+          </tr>
+          <tr>
+            <td colspan="6">Total  des Actifs</td>
+          </tr>
+        </tbody>
+      </table>
+   </div>
+      
+</body>
+</html>
